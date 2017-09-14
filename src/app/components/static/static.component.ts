e4d3b1ab1944bed86c4b32e31e5c2a3e00222f2f
r.mykhalenych <roksolanamykhalenych@gmail.com>
2017-09-14 23:00:16 +0300

    main page

--- src/app/components/static/static.component.ts
+++ src/app/components/static/static.component.ts
@@ -6,10 +6,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./static.component.css']
 })
 export class StaticComponent implements OnInit {
-
-  constructor() { }
+  subscribeImg: string;
+  contactImg: string;
+  mainVideo: string;
+  img1: string;
+  img2: string;
+  img3: string;
+  constructor() {
+    this.subscribeImg = '../assets/images/lviv-city.jpg',
+    this.mainVideo = '../assets/media/LvivNightTimelapse.mp4',
+    this.contactImg = '../assets/images/lviv-contact.jpg'
+    this.img1 = '../assets/images/news1.jpg',
+    this.img2 = '../assets/images/news2.jpg',
+    this.img3 = '../assets/images/news3.jpg'
+  }
 
   ngOnInit() {
   }
-
 }
