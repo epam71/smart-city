94b72ed469bc018285f13c41427412613d87f16a
r.mykhalenych <roksolanamykhalenych@gmail.com>
2017-09-25 16:43:10 +0300

    added scroll to top, form validation and toggle navigation

--- src/app/components/contact-us/contact-us.component.ts
+++ src/app/components/contact-us/contact-us.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import {NgForm} from '@angular/forms';
+import { Messages } from '../../models/messages.model';
+import { ContactServiceService } from '../../core/contact-service/contact-service.service';
 
 @Component({
   selector: 'app-contact-us',
@@ -6,10 +9,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contact-us.component.css']
 })
 export class ContactUsComponent implements OnInit {
+  @ViewChild('f') sendMesForm: NgForm;
   contactImg: string;
+  
   constructor() {
     this.contactImg = '../assets/images/lviv-contact.jpg'
    }
+
+   onSubmit() {
+
+    const value = this.sendMesForm.value;
+    // let messages: Messages = {
+    //   author: value.name,
+    //   email: value.email,
+    //   subject: value.subject,
+    //   text: value.message,
+    //   new: true
+    // }
+     
+    this.sendMesForm.reset();
+  }
 
   ngOnInit() {
   }
