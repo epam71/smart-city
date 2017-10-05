6c8c0c848665f3e101209cb7ddbc950e04dfb606
r.mykhalenych <roksolanamykhalenych@gmail.com>
2017-10-05 14:13:46 +0300

    nav fixed

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -9,23 +9,22 @@ import { Component, OnInit } from '@angular/core';
 export class FooterComponent implements OnInit {
   scrollUp(event): void {
     event.preventDefault();
-
-    let getHref: string = event.currentTarget.attributes.href.value;
-    let getElem: any = document.querySelector(getHref);
+    
+    let getElem: any = document.querySelector('body');
     let elemPosition: number;
-    let positionTop: number;
     let step: number;
 
     let runScrolling: any = setInterval(() => {
       elemPosition = getElem.offsetTop;
-      positionTop = window.pageYOffset;
+      //top position of element relative to the viewport
+      step = Math.round(getElem.getBoundingClientRect().top);
 
       if (step >= -2 && step <= 2) {
+        //scroll to element by its coordinates
         window.scrollTo(0, elemPosition);
         clearInterval(runScrolling);
       } else {
-        scrollBy(0, step/10);
-        step = Math.round(getElem.getBoundingClientRect().top);
+        scrollBy(0, step/20);
         this.scrollUp;
       }
     }, 10);
