ef3eaf4bce968434b327033ebc56a2c1d02b687d
r.mykhalenych <roksolanamykhalenych@gmail.com>
2017-10-06 15:59:42 +0300

    styles fixed

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -6,7 +6,6 @@ import { ContactUsComponent } from './contact-us/contact-us.component';
 import { NavigationComponent } from './navigation/navigation.component';
 import { NewsComponent } from './news/news.component';
 import { ProjectsComponent } from './projects/projects.component';
-import { SignupComponent } from './signup/signup.component';
 import { StaticComponent } from './static/static.component';
 import { routing } from './components.routes';
 import { PipesModule } from '../pipes/pipes.module';
@@ -24,7 +23,6 @@ import { PipesModule } from '../pipes/pipes.module';
       NavigationComponent,
       NewsComponent,
       ProjectsComponent,
-      SignupComponent,
       StaticComponent
     ]
 })
